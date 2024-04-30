08c408444d0660bd1a2581c082f7abd1ec524116
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-04-30 13:25:42 -0400

    fixing

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MonoGame4___Time_and_Sound
 {
@@ -9,7 +11,20 @@ namespace MonoGame4___Time_and_Sound
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        bool exploded;
+
+        float seconds;
+
+        MouseState mouseState;
+
         Rectangle window;
+        Rectangle bombRect;
+
+        SpriteFont timeFont;
+
+        SoundEffect explode;
+
+        Texture2D bombTexture;
 
         public Game1()
         {
@@ -21,10 +36,15 @@ namespace MonoGame4___Time_and_Sound
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            window = new Rectangle(0, 0, 800, 480);
+            window = new Rectangle(0, 0, 800, 500);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
 
+            bombRect = new Rectangle(50, 50, window.Width - 100, window.Height - 100);
+
+            seconds = 10;
+
+            exploded = false;
 
             base.Initialize();
         }
@@ -34,6 +54,12 @@ namespace MonoGame4___Time_and_Sound
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            bombTexture = Content.Load<Texture2D>("bomb");
+
+            timeFont = Content.Load<SpriteFont>("Time");
+
+            explode = Content.Load<SoundEffect>("explosion");
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,6 +69,25 @@ namespace MonoGame4___Time_and_Sound
 
             // TODO: Add your update logic here
 
+            mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                seconds = 10f;
+            }
+
+            seconds -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+            if (seconds <= 0 && exploded == false)
+            {
+                seconds = 0f;
+                exploded = true;
+                if (exploded = true)
+                {
+                    explode.Play();
+                }
+            }
+            
+
             base.Update(gameTime);
         }
 
@@ -51,11 +96,17 @@ namespace MonoGame4___Time_and_Sound
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
             _spriteBatch.Begin();
 
 
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+
+            _spriteBatch.DrawString(timeFont, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
+
 
             _spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
